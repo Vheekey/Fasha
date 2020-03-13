4bd00a317caefbe17f4bb2d647bfb29298a2bbe9
Vheekey <vicformidable@gmail.com>
2020-03-13 18:13:59 +0100

    creating routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', function () {
+    return view('dashboard');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+
